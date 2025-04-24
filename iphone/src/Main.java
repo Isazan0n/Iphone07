f30c0e9b5d529719a2c20c424b8b91abada880ad
Isabela <programmer.isazanon@gmail.com>
2025-04-24 15:15:47 -0300

    Primeiro commit

---
+++ iphone/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Iphone meuIphone = new Iphone();
+
+        // Testando o Reprodutor Musical
+        meuIphone.selecionarMusica("Imagine - John Lennon");
+        meuIphone.tocar();
+        meuIphone.pausar();
+
+        // Testando o Aparelho Telefônico
+        meuIphone.ligar("123456789");
+        meuIphone.atender();
+        meuIphone.iniciarCorreioVoz();
+
+        // Testando o Navegador na Internet
+        meuIphone.exibirPagina("www.exemplo.com");
+        meuIphone.adicionarNovaAba();
+        meuIphone.atualizarPagina();
+    }
+}
